77fc764a6ecb486f0a4c2b7498a72de9c523d19c
Corelocked <cedricjoshua.palapuz@gmail.com>
2024-07-12 10:03:45 +0800

    settings updated

--- app/src/main/java/com/example/unfriendster/SettingsActivity.java
+++ app/src/main/java/com/example/unfriendster/SettingsActivity.java
@@ -5,11 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private TextView nameTextView, usernameTextView, emailTextView;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +33,44 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_page);
 
         Button signoutButton = findViewById(R.id.signout_btn);
+        Button backButton = findViewById(R.id.back_btn);
+        nameTextView = findViewById(R.id.name_txt);
+        usernameTextView = findViewById(R.id.username_txt);
+        emailTextView = findViewById(R.id.email_txt);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String name = snapshot.child("name").getValue(String.class);
+                    String username = snapshot.child("username").getValue(String.class);
+                    String email = snapshot.child("email").getValue(String.class);
+
+                    nameTextView.setText(name);
+                    usernameTextView.setText("@" + username);
+                    emailTextView.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SettingsActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         signoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View v) {
-                // Show confirmation dialog
                 new AlertDialog.Builder(SettingsActivity.this)
                         .setTitle("Confirm Sign Out")
                         .setMessage("Are you sure you want to sign out?")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                // User confirmed, sign out using Firebase
                                 FirebaseAuth.getInstance().signOut();
-
-                                // Navigate to MainActivity after sign-out
                                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
                                 startActivity(intent);
                             }
@@ -39,6 +78,13 @@ public class SettingsActivity extends AppCompatActivity {
                         .setNegativeButton(android.R.string.no, null)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .show();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/unfriendster/User.java
+++ app/src/main/java/com/example/unfriendster/User.java
@@ -7,6 +7,7 @@ public class User {
     public String email;
     public String password;
 
+
     public User() {
     }
 
@@ -39,4 +40,5 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
